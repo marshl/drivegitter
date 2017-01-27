56856f83edbf41bfe24d5bf0084e7541f5f704ae
marshl <marshl@live.com.au>
2017-01-27 13:55:08 +1100

    Changed the "completed paths" code to also ignore files that have already been parsed; Changed the export mimetypes from Open Documents to MS Office 2013 documents;

--- drivegitter.py
+++ drivegitter.py
@@ -44,7 +44,7 @@ completed_paths_file = open('completed_files.txt', 'a')
 with open('completed_files.txt', 'r') as f:
     completed_paths = f.readlines()
 
-completed_paths = [x.strip() for x in completed_paths] 
+completed_paths = [x.strip() for x in completed_paths]
 
 def get_credentials():
     """Gets valid user credentials from storage.
@@ -84,7 +84,7 @@ def main():
     root_folder = drive_service.files().get(fileId=flags.root_file_id).execute()
 
     foldername = root_folder['title'].strip()
-    
+
     if vc_mode == 'git':
         output_dir = Path(output_dir, foldername)
         output_dir.mkdir(exist_ok=True)
@@ -96,9 +96,9 @@ def main():
         call(['svnadmin', 'create', repo_dir.as_posix()])
         call(['svn', 'checkout',
               'file:///{0}'.format(repo_dir.as_posix()), checkout_dir.as_posix()])
-        
+
         # Stub the revprop change hook so we  can change the date of commits
-        
+
         #open(hook_filepath, 'a').close()
         hookpath = Path(repo_dir, 'hooks')
         hook_filepath = Path(hookpath, 'pre-revprop-change.bat' if os.name == 'nt' else 'pre-revprop-change')
@@ -106,10 +106,10 @@ def main():
         f = open(hook_filepath.as_posix(), 'w')
         f.write('#!/bin/sh\n')
         f.write('exit 0\n')
-        f.close()        
+        f.close()
         if os.name != 'nt':
             call(['chmod', '0777', hook_filepath.as_posix()])
-        
+
         output_dir = checkout_dir
         os.chdir(output_dir.as_posix())
 
@@ -125,7 +125,7 @@ def process_folder(folder_id, folder_path):
     for child in children['items']:
         print("File {0}".format(child['id']))
         process_file(child['id'], folder_path)
-        
+
     completed_paths_file.write(folder_path.as_posix() + "\n")
     completed_paths_file.flush()
 
@@ -141,61 +141,74 @@ def process_file(file_id, parent_path):
     file_owner = file['owners'][0]
 
     if file['mimeType'] == 'application/vnd.google-apps.folder':
-    
+
         if file_path.as_posix() in completed_paths:
             print("Skipping directory: {0}".format(file_path.as_posix()))
             return
-    
+
         if not file_path.exists():
             file_path.mkdir()
             result = vc_add_folder(file_path, 'Added {0}'.format(filename), file['modifiedDate'], file['lastModifyingUser'], file_owner)
             if result != 0:
                 sys.exit('An error occurred when adding the folder ' + file_path.as_posix())
-        
+
         process_folder(file_id, file_path)
-    elif not os.path.isfile(file_path.as_posix()):
+        return
 
-
-        if not 'downloadUrl' in file:
-            output_mt = None
-            if file['mimeType'] == 'application/vnd.google-apps.document':
-                output_mt = 'application/vnd.oasis.opendocument.text'
-            elif file['mimeType'] == 'application/vnd.google-apps.spreadsheet':
-                output_mt = 'application/vnd.oasis.opendocument.spreadsheet'
-            else:
-                print(file['exportLinks'])
-                sys.exit('Unknown mimeType ' + file['mimeType'])
-
-            f = open(file_path.as_posix(), 'wb')
-            file_content = drive_service.files().export_media(
-                fileId=file_id, mimeType=output_mt).execute()
-            f.write(file_content)
-            f.close()
-
-            result = vc_add_file(file_path)
-            if result != 0:
-                sys.exit('An error occurred during the add')
-
-            user = file['lastModifyingUser']
-            message = 'Added {0}'.format(filename)
-            result = vc_commit_file(file_path, message, file['modifiedDate'], user, file_owner)
-            if result != 0:
-                sys.exit('An error occurred during the commit')
+    if not 'downloadUrl' in file:
+        output_mt = None
+        if file['mimeType'] == 'application/vnd.google-apps.document':
+            output_mt = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+            file_path = Path(file_path.as_posix() + '.docx')
+        elif file['mimeType'] == 'application/vnd.google-apps.spreadsheet':
+            output_mt = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+            file_path = Path(file_path.as_posix() + '.xlsx')
+        elif file['mimeType'] == 'application/vnd.google-apps.presentation':
+            output_mt = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+            file_path = Path(file_path.as_posix() + '.pptx')
         else:
-            process_file_revisions(file, parent_path, file_owner)
+            print(file['exportLinks'])
+            sys.exit('Unknown mimeType ' + file['mimeType'])
 
-        if 'trashed' in file['labels'] and file['labels']['trashed'] == True:
-            result = vc_remove_file(file_path, file, file_owner)
-            if result != 0:
-                sys.exit('An error occurred when removing the file')
+    if file_path.as_posix() in completed_paths:
+        print("Skipping file: {0}".format(file_path.as_posix()))
+        return
 
-            message = 'Removed {0}'.format(file_path.name)
-            result = vc_commit_file(
-                file_path, message, file['modifiedDate'], file['lastModifyingUser'], file_owner)
+    if 'downloadUrl' in file:
+        process_file_revisions(file, parent_path, file_owner)
 
-            if result != 0:
-                sys.exit('An error occurred then committing the changes')
+    else:
+        f = open(file_path.as_posix(), 'wb')
+        file_content = drive_service.files().export_media(
+            fileId=file_id, mimeType=output_mt).execute()
+        f.write(file_content)
+        f.close()
 
+        result = vc_add_file(file_path)
+        if result != 0:
+            sys.exit('An error occurred during the add')
+
+        user = file['lastModifyingUser']
+        message = 'Added {0}'.format(filename)
+        result = vc_commit_file(file_path, message, file['modifiedDate'], user, file_owner)
+        if result != 0:
+            sys.exit('An error occurred during the commit')
+
+
+    if 'trashed' in file['labels'] and file['labels']['trashed'] == True:
+        result = vc_remove_file(file_path, file, file_owner)
+        if result != 0:
+            sys.exit('An error occurred when removing the file')
+
+        message = 'Removed {0}'.format(file_path.name)
+        result = vc_commit_file(
+            file_path, message, file['modifiedDate'], file['lastModifyingUser'], file_owner)
+
+        if result != 0:
+            sys.exit('An error occurred then committing the changes')
+
+    completed_paths_file.write(file_path.as_posix() + "\n")
+    completed_paths_file.flush()
 
 def process_file_revisions(drive_file, parent_path, file_owner):
 
@@ -254,9 +267,9 @@ def vc_add_file(file_path):
 def vc_commit_file(file_path, message, date, modified_by_user, file_owner):
 
     email = modified_by_user['emailAddress'] if 'emailAddress' in modified_by_user else file_owner['emailAddress']
-    
+
     if vc_mode == 'git':
-    
+
         return call(['git', 'commit',
                      '--message', message,
                      '--author="{0}" <{1}>'.format(
@@ -264,32 +277,30 @@ def vc_commit_file(file_path, message, date, modified_by_user, file_owner):
                      '--date=' + date,
                      '--allow-empty',
                     file_path.as_posix()])
-    
+
     elif vc_mode == 'svn':
-        
+
         username = modified_by_user['displayName']
-        print(username)
         username = (username[0] + re.split('[. ]', username)[-1]).lower()
-        print(username)
         result = call(['svn', 'commit',
                      '--message', message,
                      '--username', username])
-                     
+
         if result != 0:
             return result
-            
+
         revision = check_output(['svnversion']).decode('utf-8').rstrip().split(':')[-1]
         result = call(['svn', 'propset', 'svn:date', '--revprop', '-r', revision, date])
         return result
-                     
+
 def vc_add_folder(file_path, message, date, modified_by_user, file_owner):
     if vc_mode == 'svn':
         vc_add_file(file_path)
         return vc_commit_file(file_path, message, date, modified_by_user, file_owner)
-    
+
     # git doesn't need to add folders, so return a success code
     return 0
-        
+
 
 def download_file(file_id, local_fd):
     """Download a Drive file's content to the local filesystem.
